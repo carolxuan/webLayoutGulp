2d66d284482b69e7e4ef266a05e69f2ab2824605
carol-liao <desire11012@gmail.com>
2021-01-26 19:07:19 +0800

    first

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+// 舊的寫法
+// gulp.task('html',function(){
+// 	return gulp.src('./app/**/*.html')
+// 	 .pipe(gulp.dest('./dist/'));
+// })
+
+// 新的寫法
+// 1
+// function copyHTML(cb){
+// 	gulp.src('./app/**/*.html')
+// 	.pipe(gulp.dest('./dist/'));
+// 	cb();
+// }
+
+//2
+function copyHTML(){
+	return gulp.src('./app/**/*.html')
+		.pipe(gulp.dest('./dist/'))
+		// 最後加上
+		.pipe(
+			browserSync.reload({
+				stream: true,
+			}),
+		);
+}
+
+function scss(){
+	return gulp.src('./app/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('.')) // 一定要加'.'
+		.pipe(gulp.dest('./dist/css'))
+		// 最後加上
+		.pipe(
+			browserSync.reload({
+				stream: true,
+			}),
+		);
+}
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+				},
+				port: 8080,
+    });
+};
+
+function watch(){
+	gulp.watch('./app/**/*.html', gulp.series(copyHTML))
+	gulp.watch('./app/scss/**/*.scss', gulp.series(scss))
+}
+
+// 同步編譯 終端機要輸入 gulp 編譯
+exports.default = gulp.series(copyHTML, scss, gulp.parallel(browser, watch));
+// exports.scss = gulp.series(scss);
+
+// 終端機要輸入 gulp copy 編譯
+// exports.copy = copyHTML;
